1f6955a6c51c64027fa5545d27f507df5c635dbb
shortcuts <vannicattec@gmail.com>
2023-01-02 23:17:38 +0100

    fix: make script more interactive

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo -e "\033[0;37m[INFO] - usage: USERNAME=my-github-username PLUGIN_NAME=my-awesome-plugin REPOSITORY_NAME=my-awesome-plugin.nvim make setup\n\033[0m"
+USAGE="\033[0;37m[INFO] - usage: USERNAME=my-github-username PLUGIN_NAME=my-awesome-plugin REPOSITORY_NAME=my-awesome-plugin.nvim make setup\n\033[0m"
+
+echo -e $USAGE
 
 if [[ -z "$USERNAME" ]]; then
     echo -e "\t> No USERNAME provided, what's your GitHub/GitLab username?"
@@ -10,25 +12,31 @@ fi
 if [[ -z "$REPOSITORY_NAME" ]]; then
     REPOSITORY_NAME=$(basename -s .git `git config --get remote.origin.url`)
 
-    while true; do
         read -rp $'\t> No REPOSITORY_NAME provided, is \033[1;32m'"$REPOSITORY_NAME"$'\033[0m good? [Y/n]\n' yn
         case $yn in
             [Yy]* ) break;;
-            [Nn]* ) exit 1;;
-            * ) break;;
+            [Nn]* ) 
+                echo -e "\t> Enter your repository name"
+                read REPOSITORY_NAME
+                ;;
+            * ) 
+                echo -e $USAGE
+                exit 1;;
         esac
-    done
 fi
 
 if [[ -z "$PLUGIN_NAME" ]]; then
-    while true; do
         read -rp $'\t> No PLUGIN_NAME provided, defaulting to \033[1;32m'"$REPOSITORY_NAME"$'\033[0m, continue? [Y/n]\n' yn
         case $yn in
             [Yy]* ) break;;
-            [Nn]* ) exit 1;;
-            * ) break;;
+            [Nn]* ) 
+                echo -e "\t> Enter your plugin name"
+                read PLUGIN_NAME
+                ;;
+            * ) 
+                echo -e $USAGE
+                exit 1;;
         esac
-    done
 
     PLUGIN_NAME=$REPOSITORY_NAME
 fi
@@ -46,5 +54,5 @@ PASCAL_CASE_PLUGIN_NAME=$(echo "$PLUGIN_NAME" | perl -pe 's/(^|-)./uc($&)/ge;s/-
 
 grep -rl "YourPluginName" .github/ plugin/ tests/ lua/ | xargs sed -i "" -e "s/YourPluginName/$PASCAL_CASE_PLUGIN_NAME/g"
 grep -rl "your-plugin-name" README.md .github/ plugin/ tests/ lua/ | xargs sed -i "" -e "s/your-plugin-name/$PLUGIN_NAME/g"
-grep -rl "YOUR_GITHUB_USERNAME" README.md .github/ .chflog/ | xargs sed -i "" -e "s/YOUR_GITHUB_USERNAME/$USERNAME/g"
+grep -rl "YOUR_GITHUB_USERNAME" README.md .github/ .chglog/ | xargs sed -i "" -e "s/YOUR_GITHUB_USERNAME/$USERNAME/g"
 grep -rl "YOUR_REPOSITORY_NAME" README.md .github/ .chglog/ | xargs sed -i "" -e "s/YOUR_REPOSITORY_NAME/$REPOSITORY_NAME/g"
