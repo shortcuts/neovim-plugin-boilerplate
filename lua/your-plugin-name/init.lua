ca726988e6711508ada1ee0e554824827d00e3be
Clément Vannicatte <vannicattec@gmail.com>
2024-09-25 22:40:06 +0200

    feat!: renew template (#22)
    ## 📃 Summary
    
    Hey, as I keep working on
    https://github.com/shortcuts/no-neck-pain.nvim, I discover new stuff,
    tools, good lua practices and some fixes here and there, so I usually
    port them to this template.
    
    Hopefully this will help someone getting started!
    
    close https://github.com/shortcuts/neovim-plugin-boilerplate/issues/21

--- lua/your-plugin-name/init.lua
+++ lua/your-plugin-name/init.lua
@@ -1,34 +1,34 @@
-local M = require("your-plugin-name.main")
-local C = require("your-plugin-name.config")
+local main = require("your-plugin-name.main")
+local config = require("your-plugin-name.config")
 
 local YourPluginName = {}
 
 --- Toggle the plugin by calling the `enable`/`disable` methods respectively.
 function YourPluginName.toggle()
     if _G.YourPluginName.config == nil then
-        _G.YourPluginName.config = C.options
+        _G.YourPluginName.config = config.options
     end
 
-    M.toggle("publicAPI_toggle")
+    main.toggle("public_api_toggle")
 end
 
 --- Initializes the plugin, sets event listeners and internal state.
-function YourPluginName.enable()
+function YourPluginName.enable(scope)
     if _G.YourPluginName.config == nil then
-        _G.YourPluginName.config = C.options
+        _G.YourPluginName.config = config.options
     end
 
-    M.enable("publicAPI_enable")
+    main.toggle(scope or "public_api_enable")
 end
 
 --- Disables the plugin, clear highlight groups and autocmds, closes side buffers and resets the internal state.
 function YourPluginName.disable()
-    M.disable("publicAPI_disable")
+    main.toggle("public_api_disable")
 end
 
 -- setup YourPluginName options and merge them with user provided ones.
 function YourPluginName.setup(opts)
-    _G.YourPluginName.config = C.setup(opts)
+    _G.YourPluginName.config = config.setup(opts)
 end
 
 _G.YourPluginName = YourPluginName
--- lua/your-plugin-name/state.lua
+++ lua/your-plugin-name/state.lua
@@ -1,36 +1,43 @@
-local D = require("your-plugin-name.util.debug")
+local log = require("your-plugin-name.util.log")
 
-local State = { enabled = false }
+local state = { enabled = false }
 
 ---Sets the state to its original value.
 ---
 ---@private
-function State:init()
+function state:init()
     self.enabled = false
 end
 
 ---Saves the state in the global _G.YourPluginName.state object.
 ---
 ---@private
-function State:save()
-    D.log("state.save", "saving state globally to _G.YourPluginName.state")
+function state:save()
+    log.debug("state.save", "saving state globally to _G.YourPluginName.state")
 
     _G.YourPluginName.state = self
 end
 
----Whether the YourPluginName is enabled or not.
+--- Sets the global state as enabled.
 ---
 ---@private
-function State:setEnabled()
+function state:set_enabled()
     self.enabled = true
+end
+
+--- Sets the global state as disabled.
+---
+---@private
+function state:set_disabled()
+    self.enabled = false
 end
 
 ---Whether the YourPluginName is enabled or not.
 ---
 ---@return boolean: the `enabled` state value.
 ---@private
-function State:getEnabled()
+function state:get_enabled()
     return self.enabled
 end
 
-return State
+return state
