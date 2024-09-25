f082dcefbe66bda4db62d21ce921fb1e41c6d3cb
shortcuts <vannicattec@gmail.com>
2024-09-25 21:56:10 +0200

    feat: state

--- lua/your-plugin-name/state.lua
+++ lua/your-plugin-name/state.lua
@@ -1,19 +1,20 @@
-local D = require("your-plugin-name.util.debug")
+local log = require("your-plugin-name.util.log")
 
-local State = { enabled = false }
+local state = { enabled = false }
+
 
 ---Sets the state to its original value.
 ---
 ---@private
-function State:init()
+function state:init()
     self.enabled = false
 end
 
 ---Saves the state in the global _G.YourPluginName.state object.
 ---
 ---@private
-function State:save()
-    D.log("state.save", "saving state globally to _G.YourPluginName.state")
+function state:save()
+    log.debug("state.save", "saving state globally to _G.YourPluginName.state")
 
     _G.YourPluginName.state = self
 end
@@ -21,7 +22,7 @@ end
 ---Whether the YourPluginName is enabled or not.
 ---
 ---@private
-function State:setEnabled()
+function state:set_enabled()
     self.enabled = true
 end
 
@@ -29,8 +30,8 @@ end
 ---
 ---@return boolean: the `enabled` state value.
 ---@private
-function State:getEnabled()
+function state:get_enabled()
     return self.enabled
 end
 
-return State
+return state
