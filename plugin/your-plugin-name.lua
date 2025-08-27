99911e6b5778dc8dc659bca590d17afed937dc3c
shortcuts <vannicattec@gmail.com>
2025-08-27 23:35:18 +0200

    fix: remove 0.7 nvim support

--- plugin/your-plugin-name.lua
+++ plugin/your-plugin-name.lua
@@ -5,11 +5,6 @@ end
 
 _G.YourPluginNameLoaded = true
 
--- Useful if you want your plugin to be compatible with older (<0.7) neovim versions
-if vim.fn.has("nvim-0.7") == 0 then
-    vim.cmd("command! YourPluginName lua require('your-plugin-name').toggle()")
-else
-    vim.api.nvim_create_user_command("YourPluginName", function()
-        require("your-plugin-name").toggle()
-    end, {})
-end
+vim.api.nvim_create_user_command("YourPluginName", function()
+    require("your-plugin-name").toggle()
+end, {})
